7fd91833f38a3b540b2f31fe2d6ac2a80bf216e9
Kognise <felix.mattick@gmail.com>
2019-05-03 12:22:12 -0400

    Make the activity indicator more native

--- components/ActivityIndicator.js
+++ components/ActivityIndicator.js
@@ -1,8 +1,12 @@
 import React, { Component } from 'react'
+import { Constants } from 'expo'
+import { ActivityIndicator as NActivityIndicator } from 'react-native'
 import { ActivityIndicator } from 'react-native-paper'
 
 export default class extends Component {
   render() {
-    return <ActivityIndicator style={{ margin: 20 }} />
+    return Constants.platform === 'android'
+      ? <ActivityIndicator style={{ margin: 20 }} />
+      : <NActivityIndicator style={{ margin: 16 }} />
   }
 }

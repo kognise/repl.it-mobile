71d6feb04ae0c2ac7c7d538063f3c6e23be2505a
Kognise <felix.mattick@gmail.com>
2019-09-02 14:27:29 -0400

    Implement folder creation

--- screens/App/Dashboard.js
+++ screens/App/Dashboard.js
@@ -3,11 +3,101 @@ import { View } from 'react-native'
 import { Menu, Searchbar, Dialog, Portal, Text, Button, withTheme } from 'react-native-paper'
 
 import { navigateSame } from '../../lib/navigation'
-import { deleteFolder } from '../../lib/network'
+import { deleteFolder, createFolder } from '../../lib/network'
 
 import Theme from '../../components/Theme'
 import NewRepl from '../../components/NewRepl'
 import Dashboard from '../../components/Dashboard'
+import FormInput from '../../components/FormInput'
+
+const NewFolder = withTheme(class extends Component {
+  state = {
+    dialogOpen: false,
+    name: '',
+    loading: false,
+    error: null
+  }
+
+  render() {
+    return (<>
+      <Menu.Item
+        title='New folder'
+        onPress={this.open}
+      />
+
+      <Portal>
+        <Dialog visible={this.state.dialogOpen} onDismiss={this.cancel}>
+          <Dialog.Title>New Folder</Dialog.Title>
+
+          <Dialog.Content>
+            {this.state.error && (
+              <Text style={{
+                color: this.props.theme.colors.error,
+                marginBottom: 10
+              }}>
+                {this.state.error}
+              </Text>
+            )}
+
+            <FormInput
+              label='Name'
+              value={this.state.name}
+              onChangeText={this.updateName}
+              disabled={this.state.loading}
+              onSubmit={this.create}
+              background={this.props.theme.colors.surface}
+            />
+          </Dialog.Content>
+
+          <Dialog.Actions>
+            <Button
+              onPress={this.cancel}
+              disabled={this.state.loading}
+            >
+              Cancel
+            </Button>
+            <Button
+              onPress={this.create}
+              loading={this.state.loading}
+              disabled={this.state.loading}
+            >
+              Create
+            </Button>
+          </Dialog.Actions>
+        </Dialog>
+      </Portal>
+    </>)
+  }
+
+  updateName = (name) => this.setState({ name })
+  open = () => this.setState({ dialogOpen: true })
+  cancel = () => {
+    this.setState({
+      dialogOpen: false,
+      loading: false,
+      error: null
+    })
+    this.props.closeMenu()
+  }
+
+  create = async () => {
+    this.setState({ loading: true })
+    try {
+      await createFolder(this.state.name, this.props.id)
+      if (!this.state.dialogOpen) return
+      const reload = this.props.navigation.getParam('reloadThis')
+
+      reload()
+      this.cancel()
+    } catch(error) {
+      if (!this.state.dialogOpen) return
+      this.setState({
+        error: error.message,
+        loading: false
+      })
+    }
+  }
+})
 
 const DeleteFolder = withTheme(class extends Component {
   state = {
@@ -98,11 +188,10 @@ export default class extends Component {
     return {
       title: navigation.getParam('name', 'Your Repls'),
       menu: (closeMenu) => (<>
-        <Menu.Item
-          title='New folder'
-          onPress={() => {
-            closeMenu()
-          }}
+        <NewFolder
+          closeMenu={closeMenu}
+          id={navigation.getParam('folderId')}
+          navigation={navigation}
         />
         {navigation.getParam('name') !== 'Unnamed' && !root ? (
           <DeleteFolder
@@ -155,6 +244,10 @@ export default class extends Component {
         </View>
       </Theme>
     )
+  }
+
+  componentWillMount() {
+    this.props.navigation.setParams({ reloadThis: this.reload })
   }
 
   updateSearch = (search) => {

b10e9415bbac15bddc4786be8ac5520d068a2e40
Kognise <felix.mattick@gmail.com>
2019-10-05 13:47:25 -0400

    Refactor Hello screen properly

--- screens/auth/Hello.js
+++ screens/auth/Hello.js
@@ -1,44 +1,49 @@
 import React from 'react'
 import { View } from 'react-native'
 import { Title, Text, Button } from 'react-native-paper'
+import { useNavigation } from 'react-navigation-hooks'
 
 import Theme from '../../components/wrappers/Theme'
 
-const Screen = () => (
-  <Theme>
-    <View
-      style={{
-        flex: 1,
-        alignItems: 'center',
-        justifyContent: 'center'
-      }}
-    >
-      <Title
-        style={{
-          fontSize: 36,
-          padding: 16,
-          textAlign: 'center'
-        }}
-      >
-        Hello, {this.props.navigation.getParam('username', 'Repl.it user')}!
-      </Title>
-      <Text
-        style={{
-          fontSize: 18,
-          textAlign: 'center',
-          maxWidth: '84%',
-          marginBottom: 20
-        }}
-      >
-        You're all set to get started
-      </Text>
+const Screen = () => {
+  const { navigate, getParam } = useNavigation()
 
-      <Button mode="contained" onPress={() => this.props.navigation.navigate('App')}>
-        Go!
-      </Button>
-    </View>
-  </Theme>
-)
+  return (
+    <Theme>
+      <View
+        style={{
+          flex: 1,
+          alignItems: 'center',
+          justifyContent: 'center'
+        }}
+      >
+        <Title
+          style={{
+            fontSize: 36,
+            padding: 16,
+            textAlign: 'center'
+          }}
+        >
+          Hello, {getParam('username', 'Repl.it user')}!
+        </Title>
+        <Text
+          style={{
+            fontSize: 18,
+            textAlign: 'center',
+            maxWidth: '84%',
+            marginBottom: 20
+          }}
+        >
+          You're all set to get started
+        </Text>
+
+        <Button mode="contained" onPress={() => navigate('App')}>
+          Go!
+        </Button>
+      </View>
+    </Theme>
+  )
+}
 
 Screen.navigationOptions = {
   title: 'Welcome'

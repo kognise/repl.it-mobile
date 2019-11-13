1b1671beabee5ce85088f95da9f8e1ecc089342c
Kognise <felix.mattick@gmail.com>
2019-11-13 11:58:51 -0500

    Fix log out button

--- screens/app/Settings.js
+++ screens/app/Settings.js
@@ -1,10 +1,16 @@
 import React, { useContext, useState, useEffect, useRef } from 'react'
 import { ScrollView, TextInput as RNTextInput } from 'react-native'
 import { List, Divider, Button, Switch, TextInput } from 'react-native-paper'
+import { useNavigation } from 'react-navigation-hooks'
 
 import { logOut } from '../../lib/network'
 import SettingsContext from '../../components/wrappers/SettingsContext'
 import Theme from '../../components/wrappers/Theme'
+
+doLogOut = async () => {
+  await logOut()
+  this.props.navigation.navigate('Auth')
+}
 
 const Screen = () => {
   const settings = useContext(SettingsContext)
@@ -22,6 +28,12 @@ const Screen = () => {
     if (!isNaN(parsed)) settingsRef.current.setIndentSize(parsed)
   }, [tempIndentSize])
 
+  const { navigate } = useNavigation()
+  const doLogOut = async () => {
+    await logOut()
+    navigate('Auth')
+  }
+
   return (
     <Theme>
       <ScrollView>
@@ -36,7 +48,7 @@ const Screen = () => {
               />
             )}
           />
-          <Button mode="outlined" style={{ margin: 16, marginTop: 8 }} onPress={this.doLogOut}>
+          <Button mode="outlined" style={{ margin: 16, marginTop: 8 }} onPress={doLogOut}>
             Log out
           </Button>
         </List.Section>
@@ -82,75 +94,3 @@ Screen.navigationOptions = {
 }
 
 export default Screen
-
-class OwO {
-  static navigationOptions = {
-    title: 'Settings'
-  }
-
-  render() {
-    return (
-      <SettingsContext.Consumer>
-        {(context) => (
-          <Theme>
-            <ScrollView>
-              <List.Section>
-                <List.Subheader>General</List.Subheader>
-                <List.Item
-                  title="Dark theme"
-                  right={() => <Switch value={context.theme} onValueChange={context.setTheme} />}
-                />
-                <Button
-                  mode="outlined"
-                  style={{ margin: 16, marginTop: 8 }}
-                  onPress={this.doLogOut}
-                >
-                  Log out
-                </Button>
-              </List.Section>
-              <Divider />
-
-              <List.Section>
-                <List.Subheader>Editor</List.Subheader>
-                <List.Item
-                  title="Soft wrapping"
-                  right={() => (
-                    <Switch value={context.softWrapping} onValueChange={context.setSoftWrapping} />
-                  )}
-                />
-                <List.Item
-                  title="Indent with spaces"
-                  right={() => (
-                    <Switch value={context.softTabs} onValueChange={context.setSoftTabs} />
-                  )}
-                />
-                <List.Item
-                  title="Indent size"
-                  right={() => (
-                    <TextInput
-                      value={context.indentSize}
-                      onChangeText={context.setIndentSize}
-                      render={(props) => (
-                        <RNTextInput
-                          {...props}
-                          style={[...props.style, { textAlign: 'center' }]}
-                          keyboardType="number-pad"
-                          maxLength={2}
-                        />
-                      )}
-                    />
-                  )}
-                />
-              </List.Section>
-            </ScrollView>
-          </Theme>
-        )}
-      </SettingsContext.Consumer>
-    )
-  }
-
-  doLogOut = async () => {
-    await logOut()
-    this.props.navigation.navigate('Auth')
-  }
-}
